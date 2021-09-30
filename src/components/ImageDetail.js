bb9890ff21c8fb9976a12fc5c9d96ad324793a4b
Akis <b_kalogeropoulos@yahoo.gr>
2021-09-30 19:26:32 +0300

    change props with title, imageSource, score

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {View, Text, StyleSheet, Image} from "react-native";
 
-const ImageDetail = (props) => {
+const ImageDetail = ({imageSource, title, score}) => {
 
 
     return (
     <View>
-        <Text>{props.title}</Text>
-        <Image source={props.imageSource}/>
+        <Text>{title}</Text>
+        <Image source={imageSource}/>
+        <Text>Image score - {score}</Text>
     </View>
     
     );
